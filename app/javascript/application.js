32986b77e9c064e522e4d0413152a34dd0fba588
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-06-05 17:40:36 +0630

    Setup react

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>Hello, world</h1>);
